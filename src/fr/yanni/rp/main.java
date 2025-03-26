755f99265ddb7df8149cc252dd501df8390cb530
yaya992 <84088736+yaya992@users.noreply.github.com>
2025-03-26 17:14:49 +0100

    1.1
    Ajoute du system de mis a jour

--- src/fr/yanni/rp/main.java
+++ src/fr/yanni/rp/main.java
@@ -1,6 +1,7 @@
 package fr.yanni.rp;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class main extends JavaPlugin {
 	
@@ -8,8 +9,19 @@ public class main extends JavaPlugin {
     public void onEnable() {
         getLogger().info("RpFramework is now enabled!");
         
+        // l'événement
         getServer().getPluginManager().registerEvents(new EventListener(this), this);
         
+        // Vérifier la mise à jour au démarrage du serveur
+        checkUpdate.checkForUpdate();
+        
+        // Vérifier la mise à jour toutes les 30 minutes (6000 ticks)
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                checkUpdate.checkForUpdate();
+            }
+        }.runTaskTimer(this, 0L, 6000L); // Exécuter toutes les 6000 ticks (30 minutes)
     }
 
     @Override
